f45df29006efd77358827532457e332829fa2e6b
lollobianco <loreb2002@gmail.com>
2022-12-16 14:53:53 +0100

    Aggiunto metodo

--- classi/movie.php
+++ classi/movie.php
@@ -8,8 +8,23 @@ class Movie{
    function __construct($title, $genre, $year){
       $this -> title = $title;
       $this -> genre = $genre;
-      $this -> year = $year; 
+      $this -> year = $year;
    }
+
+public function isHorror(){
+   
+   if($this -> genre == 'horror' || $this -> genre == 'HORROR'){
+      return "il film: $this->title è horror";
+   }  else {
+         return "il film: $this->title non è horror";
+      }  
+
 }
+
+}
+
+$arrayFilm = [];
+
+
 
 ?>
--- index.php
+++ index.php
@@ -6,31 +6,32 @@ include_once __DIR__ . '/classi/movie.php';
 
 <h1>ADD FILM TO DATABASE</h1>
 
-<form method="GET">
-  <input type="text" placeholder="title" name="title">
-  <input type="text" placeholder="genre" name="genre">
-  <input type="number" name="year">
-  <button type="submit">Send</button>
+<form method="GET" return='false'>
+  <input type="text" placeholder="Title" name="title">
+  <input type="text" placeholder="Genre" name="genre">
+  <input type="number" placeholder="Year" name="year">
+  <button type="submit">Send Data</button>
 </form>
 
 <?php
 
-$arrayFilm = [];
-
 if( !empty($_GET) && !empty( $_GET['title'] ) && !empty( $_GET['genre'] ) && !empty( $_GET['year'] ) ){
-  $title = $_GET['title'];
-  $genre = $_GET['genre'];
-  $year = $_GET['year'];
 
-  $film = new Movie( $title, $genre, $year );
-  var_dump( $film );
+   $title = $_GET['title'];
+   $genre = $_GET['genre'];
+   $year = $_GET['year'];
+
+   $film = new Movie( $title, $genre, $year );
 
    if(!in_array($film, $arrayFilm)){
       $arrayFilm[] = $film;
+   }else{
+      echo 'film già inserito';
    }
 
 }
 
 var_dump($arrayFilm);
+echo $film->isHorror();
 
 ?>
